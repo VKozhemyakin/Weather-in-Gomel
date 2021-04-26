8ea453f6588eb774e0b011977a7d5ea6d3efd637
VKozhemyakin <Prosto2Slova>
2021-04-26 18:45:56 +0300

    added TestBase

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,14 +1,19 @@
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static java.awt.SystemColor.text;
 
 public class YandexTest extends TestBase {
+    String weather = "Weather in Gomel";
+
     @Test
     public void searchWeather (){
 
         $x("//input[@id='text']").val("Weather in Gomel").pressEnter();
         $x("//div[@class='OrganicTitle-LinkText organic__url-text']").click();
+
 
         sleep(5000);
 
