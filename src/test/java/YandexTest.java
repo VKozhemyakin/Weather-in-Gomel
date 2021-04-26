6110a8f89029ed4e56cfc5c263da5e8e4a4864b9
VKozhemyakin <Prosto2Slova>
2021-04-26 16:07:19 +0300

    added TestBase

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,14 +1,11 @@
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import tests.TestBase;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class YandexTest {
+public class YandexTest extends TestBase {
     @Test
     public void searchWeather (){
-
-        Configuration.startMaximized = true;
-        open("https://yandex.by/");
 
         $x("//input[@id='text']").val("Weather in Gomel").pressEnter();
         $x("//div[@class='OrganicTitle-LinkText organic__url-text']").click();
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,16 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+
+        @BeforeAll
+
+    static void setup(){
+            Configuration.startMaximized = true;
+            open("https://yandex.by/");
+        }
+}
